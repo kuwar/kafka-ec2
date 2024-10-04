6a008bc31c303f86aea6b1d5827331d39d5fd891
Shaurave <kuwarsaurav21@gmail.com>
2024-10-04 16:00:16 +0200

    log added for each server up scripts

--- scripts/prometheus_grafana_server_up.sh
+++ scripts/prometheus_grafana_server_up.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+exec > >(tee /var/log/prometheus_grafana_server_up.log|logger -t user-data -s 2>/dev/console) 2>&1
+echo "Hello from $(hostname -f)!"
+
 PROMETHEUS_HOME="/etc/prometheus"
 
 # move prometheus.yml
-sudo mv /home/ec2-user/prometheus.yml $PROMETHEUS_HOME/prometheus.yml
+sudo mv /home/ec2-user/prometheus.yml $PROMETHEUS_HOME/prometheus.yml || { echo "prometheus.yml move failed"; exit 1; }
 # Create Prometheus service
-sudo mv /home/ec2-user/prometheus.service /etc/systemd/system/prometheus.service
+sudo mv /home/ec2-user/prometheus.service /etc/systemd/system/prometheus.service || { echo "prometheus.service move failed"; exit 1; }
 
 sudo systemctl daemon-reload
 sudo systemctl start prometheus
